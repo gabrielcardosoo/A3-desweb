08c68424d8b812fff244c1db623acd4fc972dc5d
Gabriel Cardoso <gabriel.cardoso@tooq.com.br>
2024-09-26 23:08:39 -0300

    feat: implementa a estrutura inicial do site

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/Button.css'
+
+function Button() {
+  return (
+    <div>
+      <button>Veja mais</button>
+    </div>
+  );
+}
+
+export default Button;
---
+++ frontend/src/components/Description.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/Description.css';
+
+function Description() {
+  return (
+    <div>
+      <p className='league-spartan'>CALCULE SUA DIETA EM  SEGUNDOS 7000 !</p>
+    </div>
+  );
+}
+
+export default Description;
