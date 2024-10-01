4f74bf3857fb06b3c5479555b6a6afb19b65ef99
Gabriel Silva Cardoso - 822147931 <822147931@ulife.com.br>
2024-10-01 20:41:34 -0300

    feat: Ajuste de estilização navbar e favicon

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -55,7 +55,7 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className={`collapse navbar-collapse justify-content-center ${isMenuOpen ? 'show' : ''}`} id="navbarNav">
-          <ul className="navbar-nav">
+        <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link" href="#" onClick={refreshPage}>HOME</a>
             </li>
