73cd008bf253ae5c46c9953621fab339da9583c3
Gustavo Colpaert <gustavocolpaertt@gmail.com>
2024-10-05 19:39:46 -0300

    Core: Adicionando componentes de integrantes

---
+++ frontend/src/components/Integrant_components.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style/Integrant_components.css';
+
+function Calc() {
+  return (
+    <div id="integ"className="integ-container">
+      <h1>INTEGRANTES</h1>
+    </div>
+  );
+}
+
+export default Calc;
+    
