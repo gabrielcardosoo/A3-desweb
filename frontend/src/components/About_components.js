0df6f139e8acad3423562a2c4ca334a885e59d2e
Danillo Vidal <vidaldanillo08@gmail.com>
2024-10-05 19:30:36 -0300

    feat: Estilização do componente about

--- frontend/src/components/About_components.js
+++ frontend/src/components/About_components.js
@@ -2,12 +2,24 @@ import React from 'react';
 import './style/About_components.css'; // Corrigido para o nome correto do arquivo CSS
 
 function About() {
-  return (
-    <div id="about"className="about-container">
-      <h1>Sobre</h1>
-      <p>Informações sobre o projeto...</p>
-    </div>
-  );
+  return (
+    <div id="about" className="about-container">
+      <div className="about-header">
+        <h1 className="about-title">Sobre Nós</h1>
+      </div>
+      <div className="about-content">
+            <div className="content-header">
+                <i className="fa-solid fa-bars" style={{ color: '#404040', fontSize: '20px', marginRight: '10px' }}></i>
+                <h2>October 12th, 2013</h2>
+            </div>
+        <p>
+          It was a cold Saturday morning. I was at a conference in Germany and started to feel very sick. I could barely
+          move, but I had to take a plane to speak at another conference in Spain. In the middle of the flight, I called
+          the flight attendant and said I needed help. The airplane landed, and I left there in an ambulance.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
